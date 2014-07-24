6e43f7fdd6dae9860574775ac2a5b5e4138a6414
Dmitriy Tyurin <fobia3d@gmail.com>
2014-07-24 06:16:03 +0400

    add tests

---
+++ tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * bootstrap.php file
+ *
+ * @author     Dmitriy Tyurin <fobia3d@gmail.com>
+ * @copyright  Copyright (c) 2014 Dmitriy Tyurin
+ */
+
+
+$logger = require_once __DIR__ . '/vendor/autoload.php';
+
+/* @var $logger \Composer\Autoload\ClassLoader */
+$logger->addPsr4("Fobia\\DataBase\\", __DIR__ . "/../");
+
+echo "ok";
