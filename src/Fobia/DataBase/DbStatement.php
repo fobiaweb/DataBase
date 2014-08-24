12ee48418d05bd22e2de318e6c2c36b7c319957a
Dmitriy Tyurin <fobia3d@gmail.com>
2014-08-24 05:13:25 +0400

    добавлен  parsePlaceholder

--- src/Fobia/DataBase/DbStatement.php
+++ src/Fobia/DataBase/DbStatement.php
@@ -52,6 +52,62 @@ class DbStatement extends PDOStatement
     }
 
     /**
+     * ?# - поля таблицы
+     * ?d - целочисленые параметры
+     * ?f - дробные параметры
+     * ?s - строка
+     * ?a - масив
+     *
+     * @param type $query
+     */
+    public function parsePlaceholder($query, array $params = array() )
+    {
+        if ( substr_count ($query, '?') != count($params) ) {
+            trigger_error("Не верное количество параметров", E_USER_ERROR);
+        }
+        return preg_replace_callback('/\?([#avsnd])/', function($m) use (&$params) {
+            $db = $this->connection->quote($string);
+            $val = array_shift($params);
+
+            // Столбцы
+            if ($m[1] == '#') {
+                $val = (array) $val;
+                array_walk($val, function(&$v) use ($db) {
+                    $v = $db->quoteIdentifier($v);
+                });
+                return implode(", ", $val);
+            }
+            // масив значений
+            if ($m[1] == 'a') {
+                array_walk($val, function(&$v) use($db) {
+                    $v = $db->connection->quote($v);
+                });
+                return implode(", ", $val);
+            }
+            // масив типа "ключ = значение"
+            if ($m[1] == 'v') {
+                $str = "";
+                foreach ($val as $k => $v) {
+                    $str .= "`{$k}` = '{$v}', ";
+                }
+                return substr($str, 0, -2);
+            }
+            // Прямая вставка строки
+            if ($m[1] == 's') {
+                return $val;
+            }
+            // Целое число
+            if ($m[1] == 'n') {
+                return ($val) ? $db->connection->quote($val) : 'NULL';
+            }
+            // Целое число
+            if ($m[1] == 'd') {
+                return (int) $val;
+            }
+        }, $query);
+    }
+
+    /**
      * Выводит асс. масив, где ключи это $key, а значение в зависимости от $columns
      * - [null] первое поле строки, исключая при этом поле $key
      * - [string] поле $columns
