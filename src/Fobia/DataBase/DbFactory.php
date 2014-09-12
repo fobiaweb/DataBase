6beb96aeb935ccdd64ae58b9e75e66bf86e850bd
Dmitriy Tyurin <fobia3d@gmail.com>
2014-09-12 15:51:09 +0400

    ---

--- src/Fobia/DataBase/DbFactory.php
+++ src/Fobia/DataBase/DbFactory.php
@@ -4,7 +4,7 @@
  *
  * @author     Dmitriy Tyurin <fobia3d@gmail.com>
  * @copyright  Copyright (c) 2014 Dmitriy Tyurin
- * @version    $Id$
+ * @version    $Id: Fri Sep 12 15:10:29 2014 +0400$
  *
  * This source file is subject to the MIT license that is bundled
  * with this source code in the file LICENSE.
